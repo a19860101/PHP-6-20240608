28e9e24f0651ac00fddc384ea7cec35d4e23d956
Zac <a19860101@gmail.com>
2024-06-15 14:25:02 +0800

    20240615

--- students/index.php
+++ students/index.php
@@ -4,7 +4,6 @@
 
     $result = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
-    var_dump($result);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,5 +14,45 @@
 </head>
 <body>
     <h1>學員通訊錄</h1>
+
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>姓名</th>
+            <th>電話</th>
+            <th>EMAIL</th>
+            <th>性別</th>
+            <th>科目</th>
+            <th>備註</th>
+        </tr>
+    <?php 
+        foreach($result as $student){ 
+        extract($student);
+    ?>
+        <tr>
+            <td><?php echo $id;?></td>
+            <td><?php echo $student['name'];?></td>
+            <td><?php echo $phone;?></td>
+            <td><?php echo $email;?></td>
+            <td><?php echo $gender;?></td>
+            <td><?php echo $course;?></td>
+            <td><?php echo $comment;?></td>
+        </tr>
+    <?php } ?>
+
+    </table>
+    <?php
+        // foreach($result as $student){
+            // extract($student);
+            // echo "<div>{$student['name']}</div>";
+            // echo "<div>{$student['email']}</div>";
+            // echo "<div>{$student['phone']}</div>";
+            // echo "<div>{$student['gender']}</div>";
+            // echo "<div>{$name}</div>";
+            // echo "<div>{$email}</div>";
+            // echo "<div>{$phone}</div>";
+            // echo "<div>{$gender}</div>";
+        // }
+    ?>
 </body>
 </html>
