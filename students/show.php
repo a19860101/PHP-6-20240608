00b797af73dd14d33889f5d2f477b8715acf4353
Zac <a19860101@gmail.com>
2024-06-15 15:59:39 +0800

    20240615

--- students/show.php
+++ students/show.php
@@ -14,6 +14,25 @@
     $stmt->execute([$id]);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
+    extract($result);
     print_r($result);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $result['name'];?>個人資料</title>
+</head>
+<body>
+    <ul>
+        <li>姓名：<?php echo $result['name']; ?></li>
+        <li>電話：<?php echo $result['phone']; ?></li>
+        <li>Email：<?php echo $result['email']; ?></li>
+        <li>性別：<?php echo $gender; ?></li>
+        <li>科目：<?php echo $result['course']; ?></li>
+        <li>備註：<?php echo $result['comment']; ?></li>
+    </ul>
+</body>
+</html>
 
