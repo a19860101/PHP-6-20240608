df1621e3f5a77cd1e56e81ca7cb00d7ffe5214c7
Zac <a19860101@gmail.com>
2024-06-22 12:27:22 +0800

    20240622

--- filesystem/upload.php
+++ filesystem/upload.php
@@ -1,9 +1,12 @@
 <?php
     // var_dump($_FILES);
     // var_dump($_FILES['img']);
-    echo '<div>'.$_FILES['img']['name'].'</div>';
-    echo '<div>'.$_FILES['img']['full_path'].'</div>';
-    echo '<div>'.$_FILES['img']['type'].'</div>';
-    echo '<div>'.$_FILES['img']['tmp_name'].'</div>';
-    echo '<div>'.$_FILES['img']['error'].'</div>';
-    echo '<div>'.$_FILES['img']['size'].'</div>';
+    // echo '<div>'.$_FILES['img']['name'].'</div>';
+    // echo '<div>'.$_FILES['img']['full_path'].'</div>';
+    // echo '<div>'.$_FILES['img']['type'].'</div>';
+    // echo '<div>'.$_FILES['img']['tmp_name'].'</div>';
+    // echo '<div>'.$_FILES['img']['error'].'</div>';
+    // echo '<div>'.$_FILES['img']['size'].'</div>';
+    
+    extract($_FILES['img']);
+    move_uploaded_file($tmp_name,"images/{$name}");
