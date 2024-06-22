a69599bf08071d97d3b268e2abc9d858da65211f
Zac <a19860101@gmail.com>
2024-06-22 11:23:53 +0800

    20240622

--- students_function/function.php
+++ students_function/function.php
@@ -23,3 +23,19 @@
         return $result;
 
     }
+    function store($request){
+        extract($request);
+        $sql = 'INSERT INTO students(name,phone,email,gender,course,comment)VALUES(?,?,?,?,?,?)';
+        $stmt = db()->prepare($sql);
+        $course = implode(',',$course);
+        $name = check($name);
+        $phone = check($phone);
+        $email = check($email);
+        $stmt->execute([$name,$phone,$email,$gender,$course,$comment]);
+    }
+    function check($input){
+        $input = trim($input); //去除前後空白字元
+        $input = stripslashes($input); //去除反斜線
+        $input = htmlspecialchars($input); //將特殊字元實體化
+        return $input;
+    }
--- students_function/store.php
+++ students_function/store.php
@@ -1,12 +1,6 @@
 <?php
-    include('db.php');
-    extract($_REQUEST);
-
-    $sql = 'INSERT INTO students(name,phone,email,gender,course,comment)VALUES(?,?,?,?,?,?)';
-    $stmt = $pdo->prepare($sql);
-    $course = implode(',',$course);
-    $stmt->execute([$name,$phone,$email,$gender,$course,$comment]);
-
+    include('function.php');
+    store($_REQUEST);
 
     // header('location:index.php');
     echo '<script>alert("資料已新增")</script>';
