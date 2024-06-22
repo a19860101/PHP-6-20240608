0ed1fbb3161ffa3a5eddbbaa6dc21e56840ca025
Zac <a19860101@gmail.com>
2024-06-22 12:16:36 +0800

    20240622

---
+++ filesystem/upload.php
@@ -0,0 +1,9 @@
+<?php
+    // var_dump($_FILES);
+    // var_dump($_FILES['img']);
+    echo '<div>'.$_FILES['img']['name'].'</div>';
+    echo '<div>'.$_FILES['img']['full_path'].'</div>';
+    echo '<div>'.$_FILES['img']['type'].'</div>';
+    echo '<div>'.$_FILES['img']['tmp_name'].'</div>';
+    echo '<div>'.$_FILES['img']['error'].'</div>';
+    echo '<div>'.$_FILES['img']['size'].'</div>';
