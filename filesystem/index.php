13c6c8ac784ae2ffcecba42767144172421749a1
Zac <a19860101@gmail.com>
2024-06-22 15:22:21 +0800

    20240622

--- filesystem/index.php
+++ filesystem/index.php
@@ -10,6 +10,14 @@
         <input type="file" name="img">
         <input type="submit" value="上傳圖片">
     </form>
+    <?php
+        $imgs = glob('images/*');
+    ?>
+    <div>
+        <?php foreach($imgs as $img){ ?>
+            <img src="<?php echo $img;?>" alt="" width="200">
+        <?php } ?>
+    </div>
     <?php //echo md5(time()); ?>
     <?php //echo uniqid(); ?>
 </body>
