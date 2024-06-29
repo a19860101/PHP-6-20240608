27013f4cd358f50700ac341c3ba5dbed23ae5434
Zac <a19860101@gmail.com>
2024-06-29 12:21:51 +0800

    20240629

--- member/function.php
+++ member/function.php
@@ -6,7 +6,19 @@
         會員登出 logout
     */
     include('db.php');
+    function now(){
+        date_default_timezone_set('Asia/Taipei');
+        $now = date('Y-m-d H:i:s');
+        return $now;
+    }
     function store($request){
         extract($request);
-        print_r($request);
+        $password = password_hash($password,PASSWORD_DEFAULT);
+        $sql = 'INSERT INTO users(email,password,created_at,updated_at)VALUES(?,?,?,?)';
+        try{
+            $stmt = db()->prepare($sql);
+            $stmt->execute([$email,$password,now(),now()]);
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
     }
--- member/store.php
+++ member/store.php
@@ -1,3 +1,6 @@
 <?php
     include('function.php');
     store($_REQUEST);
+
+    echo '<script>alert("註冊成功，請重新登入")</script>';
+    header('refresh:0;url=index.php');
