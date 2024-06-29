a78f51766e6aab1b8bad796a37c8ce4d6ab663c9
Zac <a19860101@gmail.com>
2024-06-29 14:53:54 +0800

    20240629

--- member/store.php
+++ member/store.php
@@ -1,8 +1,12 @@
 <?php
     include('function.php');
     $result = store($_REQUEST);
+    extract($result);
+    // print_r($result);
 
-    print_r($result);
-
-    // echo '<script>alert("註冊成功，請重新登入")</script>';
-    // header('refresh:0;url=index.php');
+    echo '<script>alert("'.$status.'")</script>';
+    if($errCode == 1){
+        header('refresh:0;url=register.php');
+    }else{
+        header('refresh:0;url=index.php');
+    }
