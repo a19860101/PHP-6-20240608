a2e05fa0e847f25d6ca6d177eb13ed72ed61cd91
Zac <a19860101@gmail.com>
2024-07-06 11:53:47 +0800

    20240706

--- class/basic.php
+++ class/basic.php
@@ -33,8 +33,8 @@
 class Hero {
     // 屬性
     public $specific;
-    public $name;
-    public $life = 100;
+    private $name = 'John';
+    protected $life = 100;
     public $attack;
     public $effect;
     public $level;
@@ -46,30 +46,48 @@ class Hero {
         }
         echo $this->attack;
     } 
+    function test(){
+        $this->name = 'JOHN';
+        echo $this->name;
+    }
 }
+$user3 = new Hero;
+$user3->test();
+
 $user1 = new Hero;
 $user1->specific = '刺客';
-$user1->name = '閃電俠';
-$user1->life -= 80; 
+// $user1->name = '閃電俠';
+// $user1->life -= 80; 
 $user1->attack = 50;
 // $user1->skill();
 // echo $user1->life;
 
 $user2 = new Hero;
 $user2->specific = '法師';
-$user2->name = '伊格';
-$user2->life = 100;
+// $user2->name = '伊格';
+// $user2->life = 100;
 $user2->attack = 20;
 // $user2->skill();
 
 
 class NPC extends Hero {
     public $attack = 16;
+    function test(){
+        $this->life = 120;
+        return $this->life;
+    }
 }
 
 $npc1 = new NPC;
 $npc1->specific = '路人';
-$npc1->name = '店員';
-$npc1->life -= 88;
+// $npc1->name = '店員';
+// $npc1->life -= 88;
 $npc1 -> skill();
 print_r($npc1);
+echo $npc1->test();
+
+/* 
+    public 可用在所有地方
+    private 只可使用在自己的類別內
+    protected 可使用在類別內
+*/
